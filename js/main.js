f91a4ecc400c00d563f96d9236e699f35fdd80f1
TNTnolik <tntnolik@gmail.com>
2021-07-31 22:24:01 +1000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let textID = 0;
+let timer = 20
+console.log(text.length);
+
+$(document).ready(function() {
+	for(var t in type){
+		$("#"+t).text(type[t]);
+	};
+	for(var a in admin){
+		$("#"+a).text(admin[a]);
+	}
+	$("#title").text(text[0]["title"]);
+	$("#text").text(text[0]["text"]);
+	textID = 1;
+
+	var _Seconds = timer ,int;
+	int = setInterval(function() { 
+	  if (_Seconds > 0) {
+	    _Seconds--;
+	  } else {
+	  	if(textID==text.length){
+	  		$("#title").text(text[0]["title"]);
+			$("#text").text(text[0]["text"]);
+	  		textID = 1;
+	  	} else {
+	  		$("#title").text(text[textID]["title"]);
+			$("#text").text(text[textID]["text"]);
+			textID++;
+	  	}
+	    _Seconds = timer;
+	  }
+	}, 1000);
+});
